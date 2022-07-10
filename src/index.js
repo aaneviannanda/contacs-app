d18fb2cbe9cc8dc52b715cb1ce74fea27737a2fe
Aan Evian Nanda <71364724+aaneviannanda@users.noreply.github.com>
2022-07-10 13:50:38 +0700

    add component and style contacts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
-const element = <h1>Hello, World!</h1>;
-
+import ContactApp from './components/ContactApp';
+ 
+// Import Style
+import './styles/style.css';
+ 
 const root = createRoot(document.getElementById('root'));
-root.render(element);
-
+root.render(<ContactApp />);
