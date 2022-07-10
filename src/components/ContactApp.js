8ac215c660fe754ff3f08760338f3bccc50bfe28
Aan Evian Nanda <71364724+aaneviannanda@users.noreply.github.com>
2022-07-10 13:57:44 +0700

    refactor code ui component with ES6

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ContactList from './ContactList';
-import { getData } from '../data/data';
+import { getData } from '../utils/data';
  
-function ContactApp() {
- const contacts = getData();
- 
- return (
-   <div className="contact-app">
-     <h1>Daftar Kontak</h1>
-     <ContactList contacts={contacts} />
-   </div>
- );
+const ContactApp  = () => {
+    const contacts = getData();
+    
+    return (
+    <div className="contact-app">
+        <h1>Daftar Kontak</h1>
+        <ContactList contacts={contacts} />
+    </div>
+    );
 }
  
 export default ContactApp;
