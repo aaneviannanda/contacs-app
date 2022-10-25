04c827bfc61410b5e0f3d42dfb14a215c77adca4
Aan Evian Nanda <71364724+aaneviannanda@users.noreply.github.com>
2022-10-25 20:02:04 +0700

    implementation react context

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,16 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaHome, FaPlusCircle, FaSignOutAlt } from 'react-icons/fa';
 import PropTypes from 'prop-types';
+import { LocaleConsumer } from '../contexts/LocaleContext';
  
 const Navigation = ( {logout, name} ) => {
   return (
-    <nav className="navigation">
-      <ul>
-        <li><Link to="/"><FaHome /></Link></li>
-        <li><Link to="/add"><FaPlusCircle /></Link></li>
-        <li><button onClick={logout}>{name} <FaSignOutAlt /></button></li>
-      </ul>
-    </nav>
+    <LocaleConsumer>
+      {
+        ({ locale, toggleLocale}) => {
+          return (
+            <nav className="navigation">
+              <ul>
+                <li><button onClick={toggleLocale}>{locale === 'id' ? 'en' : 'id'}</button></li>
+                <li><Link to="/"><FaHome /></Link></li>
+                <li><Link to="/add"><FaPlusCircle /></Link></li>
+                <li><button onClick={logout}>{name} <FaSignOutAlt /></button></li>
+              </ul>
+            </nav>
+          )
+        }
+      }
+    </LocaleConsumer>
   );
 }
 
